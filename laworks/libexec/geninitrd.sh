495ecc24b9f8cb2ddd0bcb0b824f731dac7d8299
leo.c.wu <naughtyboywx@yeah.net>
2017-12-23 11:31:03 -0500

    restructure files

---
+++ laworks/libexec/geninitrd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "x$LAROOT" == "x" ]; then
+    echo "LAROOT is not set, ensure the environment has been set up"
+    exit 1
+fi
+
+LAHOME=$LAROOT
+SCRIPTSDIR=$LAHOME/scripts/misc
+SCRIPT=laworks.hook
+HOOKDIR=/usr/share/initramfs-tools/hooks
+DESTDIR=/var/lib/tftpboot/
+INITRD=initrd.gz
+
+cp $SCRIPTSDIR/$SCRIPT $HOOKDIR
+
+mkinitramfs -o  $DESTDIR/$INITRD
+
+rm -fr $HOOKDIR/$SCRIPT
